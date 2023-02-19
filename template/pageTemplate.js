35108348115340a59dc7dd62ccf0c8910437d3b5
Yuri3358 <yurialmeida917@gmail.com>
2023-02-19 20:32:20 -0300

    style: adding h1 to header

--- template/pageTemplate.js
+++ template/pageTemplate.js
@@ -1,5 +1,7 @@
 const pageTemplate = `
-    <header>Discord Webhook Messenger</header>
+    <header>
+        <h1>Discord Webhook Messenger</h1>
+    </header>
     <div id="content-box">
         <label for="token">Token da API</label>
         <input id="token" autocomplete=off v-model="apiToken">
@@ -8,9 +10,8 @@ const pageTemplate = `
         <input id="channel" autocomplete=off v-model="channelId">
 
         <label for="message">Mensagem</label>
-        <textarea id="message" autocomplete=off v-model="message">
-
-        <button class="btn btn-success">Enviar Mensagem</button>
+        <textarea id="message" autocomplete=off v-model="message"></textarea>
+        <button class="btn btn-success" @click="sendMessage">Enviar Mensagem</button>
     </div>
 `
 
